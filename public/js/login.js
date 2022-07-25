f98e062e29dc7abce86da6d49dd2ea26d144cbee
Takatoshi-Miura <takatoshi052031@gmail.com>
2022-07-25 19:50:52 +0900

    [Add] ログイン画面にUIを追加

--- public/js/login.js
+++ public/js/login.js
@@ -3,8 +3,10 @@ var LOGIN = {};
 // 文字列定義
 LOGIN.title = "";
 LOGIN.description = "課題解決型アプリSportsNote(Web版)のログインページです。";
+LOGIN.mailAddress = "";
+LOGIN.password = "";
 
-// 初期化
+/* 初期化 */
 LOGIN.init = function() {
     // ページ情報設定
     LOGIN.title = "ログイン｜" + COM.appName;
@@ -17,6 +19,49 @@ LOGIN.init = function() {
     document.querySelector('meta[property="og:image"]').setAttribute("content", COM.appImagePath);
     document.querySelector('meta[property="og:site_name"]').setAttribute("content", COM.appName);
 
-    // 処理
-    
+    document.forms.loginForm.mail.value = LOGIN.mailAddress;
+};
+
+/*　ログインボタンの処理 */
+LOGIN.actionLogin = function() {
+    // 入力値を残す
+    LOGIN.mailAddress = document.forms.loginForm.mail.value;
+    LOGIN.password = document.forms.loginForm.password.value;
+
+    // 入力チェック
+    if (LOGIN.mailAddress == "" || LOGIN.password == "") {
+        alert("メールアドレスまたはパスワードが入力されていません。");
+        return;
+    }
+
+    // ログイン処理
+    firebase.auth().signInWithEmailAndPassword(LOGIN.mailAddress, LOGIN.password)
+    .then((userCredential) => {
+        var user = userCredential.user;
+        setTimeout(function() {
+            window.location.href = './task_list.html';
+        }, 100);
+    })
+    .catch((error) => {
+        var errorCode = error.code;
+        var errorMessage = error.message;
+        alert(errorMessage);
+    });
+};
+
+/*　パスワードリセットボタンの処理 */
+LOGIN.actionPasswordReset = function() {
+    // 入力値を残す
+    LOGIN.mailAddress = document.forms.loginForm.mail.value;
+
+    // 入力チェック
+    if (LOGIN.mailAddress == "") {
+        alert("メールアドレスが入力されていません。");
+        return;
+    }
+
+    // パスワードリセットメール送信処理
+    if (confirm('パスワードリセットメールを送信しますか？')) {
+        
+    }
 };
---
+++ public/lib/firebase.js
@@ -0,0 +1,13 @@
+// Your web app's Firebase configuration
+var firebaseConfig = {
+  apiKey: "AIzaSyDPOIelluKaZv8--wAksjl9CZrrOdqYJ40",
+  authDomain: "awesome-wares-264812.firebaseapp.com",
+  databaseURL: "https://awesome-wares-264812.firebaseio.com",
+  projectId: "awesome-wares-264812",
+  storageBucket: "awesome-wares-264812.appspot.com",
+  messagingSenderId: "850229978924",
+  appId: "1:850229978924:web:8c88bd3497c24449766e89"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
