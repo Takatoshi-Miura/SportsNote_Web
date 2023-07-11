7c8a61ac1005f0e7ad6ac6202d2f20be8b67826e
Takatoshi-Miura <takatoshi052031@gmail.com>
2023-07-11 21:26:20 +0900

    初期コミット

---
+++ sportsnote/src/main/java/com/it6210/sportsnote/SportsnoteApplication.java
@@ -0,0 +1,13 @@
+package com.it6210.sportsnote;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SportsnoteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SportsnoteApplication.class, args);
+	}
+
+}
---
+++ sportsnote/src/test/java/com/it6210/sportsnote/SportsnoteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.it6210.sportsnote;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SportsnoteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
